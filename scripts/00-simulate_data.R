445b79bcc9b6bd470a8aec5d1bfcfd426776f91e
Jin Zhang <kry.zhang@mail.utoronto.ca>
2024-09-24 02:02:16 -0400

    Change script 0 to simulate shootings and firearm discharges data
    This commit includes the following:
    - Changed starter folder 'scripts/00-simulate_data.R' code to simulate a dataset mimicking shootings and firearm discharges incidents in Toronto.
    - The script generates data for event types ('shooting_event' and 'firearm_discharge'), dates (2018-2023), injuries, deaths, and neighbourhoods.
    - Data saved to '~/Desktop/Shootings-Firearm-Discharges-Research/data/raw_data/simulated_shooting_data.csv'.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,54 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates Toronto Shootings and Firearm Discharges data
+# Author: Jin Zhang
+# Date: 23 September 2024
+# Contact: kry.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
+set.seed(1008184288)
 #### Simulate data ####
-# [...ADD CODE HERE...]
 
+start_date <- as.Date("2018-01-01")
+end_date <- as.Date("2023-12-31")
+
+number_of_dates <- 1000
+
+event_types <- sample(c("shooting_event", "firearm_discharge"), 
+                      number_of_dates, replace = TRUE)
+
+# Simulated date
+dates <- as.Date(
+  runif(n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)),
+  origin = "1970-01-01"
+)
+
+#Simulated community
+neighbourhoods <- c("Downtown", "Scarborough", "North York", "Etobicoke", "East York")
+random_neighbourhoods <- sample(neighbourhoods, number_of_dates, replace = TRUE)
+
+#Simulated number of casualties and fatalities
+injuries <- ifelse(event_types == "shooting_event", rpois(number_of_dates, lambda = 1), 0)
+deaths <- ifelse(event_types == "shooting_event", rbinom(number_of_dates, 1, 0.1), 0)
+
+# Generate data
+data <- tibble(
+  dates = dates,
+  event_type = event_types,
+  neighbourhood = random_neighbourhoods,
+  injuries = injuries,
+  deaths = deaths
+)
+
+print(head(data))
+
+write.csv(data,"~/Desktop/Shootings-Firearm-Discharges-Research/data/raw_data/simulated_shooting_data.csv", row.names = FALSE)
 
 
